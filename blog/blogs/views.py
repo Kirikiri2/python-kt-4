61c7e5b99e9f39c4c8a6718b2cc4a5afc360773b
Kirikiri2 <worklisa271@gmail.com>
2025-05-22 20:55:50 +0300

    темы и содержание

---
+++ blog/blogs/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from .models import BlogPost
+from .forms import TopicForm, EntryForm
+
+def index(request):
+    """Домашняя страница приложения Learning Log"""
+    return render(request, 'blogs/index.html')
+
+def topics(request):
+    """Выводит список тем."""
+    topics = BlogPost.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'blogs/topics.html', context)
+
+def topic(request, topic_id):
+    """Выводит одну тему и все ее записи."""
+    topic = BlogPost.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'blogs/topic.html', context)
+
+
+def new_topic(request):
+    """Определяет новую тему."""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = TopicForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:topics')
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'blogs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Добавляет новую запись по конкретной теме."""
+    topic = BlogPost.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = EntryForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('blogs:topic', topic_id=topic_id)
+    # Вывести пустую или недействительную форму.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'blogs/new_entry.html', context)

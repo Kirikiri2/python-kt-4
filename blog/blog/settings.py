6529d728de4565c9acf48b20349c4d8ab0f1f2fd
Kirikiri2 <worklisa271@gmail.com>
2025-05-22 21:51:29 +0300

    финал

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blogs',
+    'users',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/users/login/'
